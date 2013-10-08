dc358e0c9317f3be085d13a90ec27cc75b993f1d
Kevin Saliou <kevin@saliou.name>
2013-10-08 21:44:17 +0200

    add setUser(), setRepo() and setFeatureSet() to GitSpatial

--- src/layer/GitSpatial.js
+++ src/layer/GitSpatial.js
@@ -1,25 +1,24 @@
 lvector.GitSpatial = lvector.GeoJSONLayer.extend({
     initialize: function(options) {
-        
+
         // Check for required parameters
         for (var i = 0, len = this._requiredParams.length; i < len; i++) {
             if (!options[this._requiredParams[i]]) {
                 throw new Error("No \"" + this._requiredParams[i] + "\" parameter found.");
             }
         }
-        
+
         // Extend Layer to create GitSpatial
         lvector.Layer.prototype.initialize.call(this, options);
-        
+
         // _globalPointer is a string that points to a global function variable
         // Features returned from a JSONP request are passed to this function
         this._globalPointer = "GitSpatial_" + Math.floor(Math.random() * 100000);
         window[this._globalPointer] = this;
-        
+
         // Create an array to hold the features
         this._vectors = [];
-        
-        
+
         if (this.options.map) {
             if (this.options.scaleRange && this.options.scaleRange instanceof Array && this.options.scaleRange.length === 2) {
                 var z = this.options.map.getZoom();
@@ -29,13 +28,25 @@ lvector.GitSpatial = lvector.GeoJSONLayer.extend({
             this._show();
         }
     },
-    
-    options: {
-        
+
+    setUser: function(user) {
+        this.options.user = user;
     },
-    
+
+    setRepo: function(repo) {
+        this.options.repo = repo;
+    },
+
+    setFeatureSet: function(featureSet) {
+        this.options.featureSet = featureSet;
+    },
+
+    options: {
+
+    },
+
     _requiredParams: ["user", "repo", "featureSet"],
-    
+
     _getFeatures: function() {
         // Build URL
         var url = "http://gitspatial.com/api/v1/" + this.options.user + // The GitHub user name
@@ -45,10 +56,8 @@ lvector.GitSpatial = lvector.GeoJSONLayer.extend({
         if (!this.options.showAll) {
             url += "&bbox=" + this.options.map.getBounds().toBBoxString(); // Build bbox geometry
         }
-        
+
         // JSONP request
         this._makeJsonpRequest(url);
-        
     }
-    
 });
